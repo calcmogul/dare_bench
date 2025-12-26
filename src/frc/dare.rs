6d8f5c786d2c2e4477904555e27ffc08b93aae36
Tyler Veness <calcmogul@gmail.com>
2025-12-26 12:45:06 -0800

    Replace SMatrix with DMatrix
    The const generic type bounds broke in a newer version of nalgebra, and
    I can't figure out how to fix them.

--- src/frc/dare.rs
+++ src/frc/dare.rs
@@ -1,8 +1,4 @@
-use nalgebra::{
-    allocator::{Allocator, SameShapeAllocator},
-    constraint::{AreMultipliable, SameNumberOfRows, ShapeConstraint},
-    ArrayStorage, Const, DefaultAllocator, DimMin, DimMinimum, SMatrix,
-};
+use nalgebra::DMatrix;
 
 // Works cited:
 //
@@ -11,27 +7,12 @@ use nalgebra::{
 //     International Journal of Control, 77:8, 767-788, 2004.
 //     DOI: 10.1080/00207170410001714988
 
-pub fn dare<const States: usize, const Inputs: usize>(
-    A: &SMatrix<f64, States, States>,
-    B: &SMatrix<f64, States, Inputs>,
-    Q: &SMatrix<f64, States, States>,
-    R: &SMatrix<f64, Inputs, Inputs>,
-) -> SMatrix<f64, States, States>
-where
-    DefaultAllocator:
-        Allocator<f64, Const<States>, Const<States>, Buffer = ArrayStorage<f64, States, States>>,
-    DefaultAllocator:
-        Allocator<f64, Const<States>, Const<Inputs>, Buffer = ArrayStorage<f64, States, Inputs>>,
-    DefaultAllocator:
-        SameShapeAllocator<f64, Const<States>, Const<States>, Const<States>, Const<States>>,
-    DefaultAllocator: Allocator<(usize, usize), DimMinimum<Const<States>, Const<States>>>,
-
-    ShapeConstraint: AreMultipliable<Const<States>, Const<Inputs>, Const<Inputs>, Const<States>>,
-    ShapeConstraint: SameNumberOfRows<Const<Inputs>, Const<Inputs>>,
-    ShapeConstraint: SameNumberOfRows<Const<States>, Const<States>, Representative = Const<States>>,
-
-    Const<States>: DimMin<Const<States>, Output = Const<States>>,
-{
+pub fn dare(
+    A: &DMatrix<f64>,
+    B: &DMatrix<f64>,
+    Q: &DMatrix<f64>,
+    R: &DMatrix<f64>,
+) -> DMatrix<f64> {
     // Implements the SDA algorithm on page 5 of [1].
 
     // A₀ = A
@@ -40,7 +21,7 @@ where
     // G₀ = BR⁻¹Bᵀ
     //
     // See equation (4) of [1].
-    let mut G_k = B * &R.cholesky().unwrap().solve(&B.transpose());
+    let mut G_k = B * &R.clone().cholesky().unwrap().solve(&B.transpose());
 
     // H₀ = Q
     //
@@ -52,7 +33,7 @@ where
         H_k = H_k1;
 
         // W = I + GₖHₖ
-        let W = SMatrix::<f64, States, States>::identity() + G_k * H_k;
+        let W = DMatrix::<f64>::identity(A.nrows(), A.ncols()) + G_k.clone() * H_k.clone();
 
         let W_solver = W.lu();
 
@@ -75,16 +56,16 @@ where
         let V_2 = W_solver.solve(&G_k.transpose()).unwrap().transpose();
 
         // Gₖ₊₁ = Gₖ + AₖV₂Aₖᵀ
-        G_k += A_k * V_2 * A_k.transpose();
+        G_k += A_k.clone() * V_2 * A_k.transpose();
 
         // Hₖ₊₁ = Hₖ + V₁ᵀHₖAₖ
-        H_k1 = H_k + V_1.transpose() * H_k * A_k;
+        H_k1 = H_k.clone() + V_1.transpose() * H_k.clone() * A_k.clone();
 
         // Aₖ₊₁ = AₖV₁
         A_k *= V_1;
 
         // while |Hₖ₊₁ − Hₖ| > ε |Hₖ₊₁|
-        if (H_k1 - H_k).norm() <= 1e-10f64 * H_k1.norm() {
+        if (H_k1.clone() - H_k).norm() <= 1e-10f64 * H_k1.norm() {
             break;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,75 @@
-#![feature(generic_const_exprs)]
-
 mod frc;
 
-use nalgebra::{
-    allocator::Allocator, ArrayStorage, Const, DefaultAllocator, DimMin, SMatrix, ToTypenum,
-};
+use nalgebra::{DMatrix, dmatrix};
 use std::time::Instant;
 
-fn discretize_ab<const States: usize, const Inputs: usize>(
-    contA: &SMatrix<f64, States, States>,
-    contB: &SMatrix<f64, States, Inputs>,
+fn discretize_ab(
+    contA: &DMatrix<f64>,
+    contB: &DMatrix<f64>,
     dt: f64,
-    discA: &mut SMatrix<f64, States, States>,
-    discB: &mut SMatrix<f64, States, Inputs>,
-) where
-    Const<{ States + Inputs }>:
-        ToTypenum + DimMin<Const<{ States + Inputs }>, Output = Const<{ States + Inputs }>>,
-    DefaultAllocator: Allocator<
-        f64,
-        Const<{ States + Inputs }>,
-        Const<{ States + Inputs }>,
-        Buffer = ArrayStorage<f64, { States + Inputs }, { States + Inputs }>,
-    >,
-{
+    discA: &mut DMatrix<f64>,
+    discB: &mut DMatrix<f64>,
+) {
+    let States = contA.nrows();
+    let Inputs = contB.ncols();
+
     // M = [A  B]
     //     [0  0]
-    let mut M = SMatrix::<f64, { States + Inputs }, { States + Inputs }>::zeros();
-    // let mut M = SMatrix::<f64, 7, 7>::zeros();
-    M.fixed_view_mut::<States, States>(0, 0).copy_from(contA);
-    M.fixed_view_mut::<States, Inputs>(0, States)
-        .copy_from(contB);
+    let mut M = DMatrix::<f64>::zeros(States + Inputs, States + Inputs);
+    M.view_mut((0, 0), (States, States)).copy_from(contA);
+    M.view_mut((0, States), (States, Inputs)).copy_from(contB);
 
     // ϕ = eᴹᵀ = [A_d  B_d]
     //           [ 0    I ]
     let phi = (M * dt).exp();
 
-    *discA = phi.fixed_view::<States, States>(0, 0).into();
-    *discB = phi.fixed_view::<States, Inputs>(0, States).into();
+    *discA = phi.view((0, 0), (States, States)).into();
+    *discB = phi.view((0, States), (States, Inputs)).into();
 }
 
 #[rustfmt::skip]
 fn init_args(
-    A: &mut SMatrix<f64, 5, 5>,
-    B: &mut SMatrix<f64, 5, 2>,
-    Q: &mut SMatrix<f64, 5, 5>,
-    R: &mut SMatrix<f64, 2, 2>,
-) {
-    let mut contA = SMatrix::<f64, 5, 5>::new(
-        0.0, 0.0, 0.0, 0.5, 0.5,
-        0.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, -1.1111111111111112, 1.1111111111111112,
-        0.0, 0.0, 0.0, -10.486221508345572, 5.782171664108812,
-        0.0, 0.0, 0.0, 5.782171664108812, -10.486221508345572,
-    );
-    let contB = SMatrix::<f64, 5, 2>::new(
-        0.0, 0.0,
-        0.0, 0.0,
-        0.0, 0.0,
-        6.664631384780125, -5.106998986026231,
-        -5.106998986026231, 6.664631384780125,
-    );
-    *Q = SMatrix::<f64, 5, 5>::new(
-        256.0, 0.0, 0.0, 0.0, 0.0,
-        0.0, 64.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 0.16, 0.0, 0.0,
-        0.0, 0.0, 0.0, 1.10803324099723, 0.0,
-        0.0, 0.0, 0.0, 0.0, 1.10803324099723,
-    );
-    *R = SMatrix::<f64, 2, 2>::new(
-        0.006944444444444444, 0.0,
-        0.0, 0.006944444444444444,
-    );
+    A: &mut DMatrix<f64>,
+    B: &mut DMatrix<f64>,
+    Q: &mut DMatrix<f64>,
+    R: &mut DMatrix<f64>) {
+    let mut contA = dmatrix![
+        0.0, 0.0, 0.0, 0.5, 0.5;
+        0.0, 0.0, 0.0, 0.0, 0.0;
+        0.0, 0.0, 0.0, -1.1111111111111112, 1.1111111111111112;
+        0.0, 0.0, 0.0, -10.486221508345572, 5.782171664108812;
+        0.0, 0.0, 0.0, 5.782171664108812, -10.486221508345572];
+    let contB = dmatrix![
+        0.0, 0.0;
+        0.0, 0.0;
+        0.0, 0.0;
+        6.664631384780125, -5.106998986026231;
+        -5.106998986026231, 6.664631384780125];
+    *Q = dmatrix![
+        256.0, 0.0, 0.0, 0.0, 0.0;
+        0.0, 64.0, 0.0, 0.0, 0.0;
+        0.0, 0.0, 0.16, 0.0, 0.0;
+        0.0, 0.0, 0.0, 1.10803324099723, 0.0;
+        0.0, 0.0, 0.0, 0.0, 1.10803324099723];
+    *R = dmatrix![
+        0.006944444444444444, 0.0;
+        0.0, 0.006944444444444444];
 
     const VELOCITY: f64 = 2.0;
     contA[(1, 2)] = VELOCITY;
 
-    discretize_ab::<5, 2>(&contA, &contB, 0.005, A, B);
+    discretize_ab(&contA, &contB, 0.005, A, B);
 }
 
 fn main() {
-    let mut A = SMatrix::<f64, 5, 5>::zeros();
-    let mut B = SMatrix::<f64, 5, 2>::zeros();
-    let mut Q = SMatrix::<f64, 5, 5>::zeros();
-    let mut R = SMatrix::<f64, 2, 2>::zeros();
+    let mut A = DMatrix::<f64>::zeros(5, 5);
+    let mut B = DMatrix::<f64>::zeros(5, 2);
+    let mut Q = DMatrix::<f64>::zeros(5, 5);
+    let mut R = DMatrix::<f64>::zeros(2, 2);
     init_args(&mut A, &mut B, &mut Q, &mut R);
 
     let start = Instant::now();
-    let S = frc::dare::<5, 2>(&A, &B, &Q, &R);
+    let S = frc::dare(&A, &B, &Q, &R);
     let end = Instant::now();
 
     println!("S = {}", S);
